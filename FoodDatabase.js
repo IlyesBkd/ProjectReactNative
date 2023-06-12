9c2527cb1b980f0706bfb8efa67c7036f06d1a68
IlyesBkd <ilyeesx@gmail.com>
2023-06-12 04:02:04 +0200

    add autocomplete working

--- FoodDatabase.js
+++ FoodDatabase.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
-import { StyleSheet, View, TextInput, Button, Text, Image, Modal } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Text, Image, Modal, FlatList, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import axios from 'axios';
 import { MealPlanContext } from './MealPlanningContext.js';
 
 const styles = StyleSheet.create({
@@ -33,6 +34,15 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 40,
   },
+  suggestionItem: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: 'lightgray', // Couleur de fond des suggestions
+    marginBottom: 2, // Espacement entre les suggestions
+  },
+  suggestionText: {
+    fontSize: 16,
+  },
 });
 
 const FoodDatabase = () => {
@@ -43,6 +53,7 @@ const FoodDatabase = () => {
   const [showPicker, setShowPicker] = useState(false);
   const [selectedMeal, setSelectedMeal] = useState('Breakfast');
   const [selectedDay, setSelectedDay] = useState('Monday');
+  const [autocompleteSuggestions, setAutocompleteSuggestions] = useState([]);
   const { mealPlan, setMealPlan, saveMealPlanToStorage } = useContext(MealPlanContext);
 
   const handleMealSelection = (meal) => {
@@ -57,6 +68,22 @@ const FoodDatabase = () => {
     const foodWithMeal = { ...food, meal: selectedMeal };
     setSelectedFoods([...selectedFoods, foodWithMeal]);
     setShowPicker(true);
+  };
+
+  const handleInputChange = async (query) => {
+    setSearchQuery(query);
+    setAutocompleteSuggestions([]);
+
+    try {
+      const response = await axios.get(
+        `https://api.edamam.com/auto-complete?app_id=190a734c&app_key=9115f8a7aefa3d9acc780dc40f6f3908&q=${query}&limit=3`
+      );
+
+      const suggestions = response.data;
+      setAutocompleteSuggestions(suggestions);
+    } catch (error) {
+      console.error('Error while fetching autocomplete suggestions:', error);
+    }
   };
 
   const handleSearch = async () => {
@@ -92,8 +119,31 @@ const FoodDatabase = () => {
       calories: Math.round(searchResults.nutrients.ENERC_KCAL),
     });
     setMealPlan(updatedMealPlan);
-    //saveMealPlanToStorage(updatedMealPlan);
     setShowPicker(false);
+  };
+
+  const handleSuggestionSelection = async (suggestion) => {
+    setSearchQuery(suggestion);
+    setAutocompleteSuggestions([]);
+
+    try {
+      const response = await fetch(
+        `https://api.edamam.com/api/food-database/v2/parser?app_id=190a734c&app_key=9115f8a7aefa3d9acc780dc40f6f3908&ingr=${suggestion}&nutrition-type=cooking`
+      );
+      const data = await response.json();
+
+      if (data.hints.length > 0) {
+        setSearchResults(data.hints[0].food);
+        setError(null);
+      } else {
+        setSearchResults(null);
+        setError('Food not found');
+      }
+    } catch (error) {
+      console.error('Error while fetching search results:', error);
+      setSearchResults(null);
+      setError('An error occurred');
+    }
   };
 
   return (
@@ -102,10 +152,25 @@ const FoodDatabase = () => {
         style={styles.searchInput}
         placeholder="Search for a food"
         value={searchQuery}
-        onChangeText={setSearchQuery}
+        onChangeText={handleInputChange}
       />
+  
+      {/* Autocomplete Suggestions */}
+      <FlatList
+        data={autocompleteSuggestions}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.suggestionItem}
+            onPress={() => handleSuggestionSelection(item)}
+          >
+            <Text>{item}</Text>
+          </TouchableOpacity>
+        )}
+      />
+  
       <Button title="Search" onPress={handleSearch} />
-
+  
       {error && <Text>{error}</Text>}
       {searchResults && (
         <View>
@@ -118,32 +183,7 @@ const FoodDatabase = () => {
           {showPicker && (
             <Modal visible={showPicker} animationType="slide">
               <View style={styles.container}>
-                <View style={styles.pickerContainer}>
-                  <Picker
-                    selectedValue={selectedMeal}
-                    onValueChange={handleMealSelection}
-                    style={styles.picker}>
-                    <Picker.Item label="Breakfast" value="Breakfast" />
-                    <Picker.Item label="Lunch" value="Lunch" />
-                    <Picker.Item label="Dinner" value="Dinner" />
-                    <Picker.Item label="Snack" value="Snack" />
-                  </Picker>
-                </View>
-                <View style={styles.pickerContainer}>
-                  <Picker
-                    selectedValue={selectedDay}
-                    onValueChange={handleDaySelection}
-                    style={styles.picker}>
-                    <Picker.Item label="Monday" value="Monday" />
-                    <Picker.Item label="Tuesday" value="Tuesday" />
-                    <Picker.Item label="Wednesday" value="Wednesday" />
-                    <Picker.Item label="Thursday" value="Thursday" />
-                    <Picker.Item label="Friday" value="Friday" />
-                    <Picker.Item label="Saturday" value="Saturday" />
-                    <Picker.Item label="Sunday" value="Sunday" />
-                  </Picker>
-                </View>
-                <Button title="Confirm" onPress={handleConfirm} />
+                {/* ... */}
               </View>
             </Modal>
           )}
